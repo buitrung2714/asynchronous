d451c2ab32c64fcf60e6e7aa62d57ae3dcba7509
Trung <dh51704388@student.stu.edu.vn>
2021-09-07 11:36:30 +0700

    loop timer in async

---
+++ test.js
@@ -0,0 +1,40 @@
+const repo = [
+  {
+    title: "content 1",
+    time: 3000,
+  },
+  {
+    title: "content 2",
+    time: 2000,
+  },
+  {
+    title: "content 3",
+    time: 4000,
+  },
+];
+
+const func = (title, time) => {
+  const promise = new Promise((resolve, reject) => {
+    if (repo.length > 0) {
+      setTimeout(() => {
+        resolve(title);
+      }, time);
+    } else reject("No data...");
+  });
+
+  return promise;
+};
+
+//Action
+const action = async () => {
+  try {
+    for (let i in repo) {
+      let { title, time } = repo[i];
+      const data = await func(title, time);
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+action();
